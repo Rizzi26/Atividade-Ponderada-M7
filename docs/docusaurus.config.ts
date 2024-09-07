82456b9c87399872c4ef9eb4d33fde934ed9f156
Rizzi26 <marco.meneguetti@sou.inteli.edu.br>
2024-09-07 15:57:37 -0300

    initial config docussauros and test deploy

---
+++ docs/docusaurus.config.ts
@@ -0,0 +1,83 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Docs Rizzi',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/docusaurus.png',
+
+  // Set the production url of your site here
+  url: 'https://rizzi-ponderada.io/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/atividade-ponderada-m7/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'Inteli-College', // Usually your GitHub org/user name.
+  projectName: 'atividade-ponderada-m7', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          routeBasePath: '/',
+          // editUrl: 'https://github.com/Inteli-College/atividade-ponderada-m7/tree/docs/setup/docs',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    // image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Rizzi-Ponderada',
+      logo: {
+        alt: 'Rizzi-Ponderada Logo',
+        src: 'img/logo.svg',
+      },
+      items: [],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()} Rizzi-Ponderada. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+
+    customCss: `
+      @media screen and (min-width: 1900px) {
+        html {
+          zoom: 60%;
+        }
+      }
+    `,
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
